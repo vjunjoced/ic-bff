8b64580da39a9bebab4947ab592d0528c07b8b7e
Junior Vasquez <jvasquez.x1@gmail.com>
2022-07-21 15:12:32 +0000

    init commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+const prueba = 'asdad';
+
+dotenv.config();
+
+function test(params) {}
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
